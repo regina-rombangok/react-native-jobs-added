12572331ef945dfd6ad859ae02c11998ebc0132a
Regina <regina.rombangok@gmail.com>
2020-12-22 19:59:25 -0500

    Add home screen

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const onPress = () => {
+    console.log('AAA');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>General Contractor Selection Process!</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text style={styles.buttonLabel}>Start</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +28,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+    margin: 16,
+  },
+  button: {
+    width: 200,
+    borderRadius: 20,
+    backgroundColor: '#3234a8',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonLabel: {
+    fontSize: 20,
+    color: 'white',
+  }
 });
